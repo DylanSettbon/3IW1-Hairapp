42d1d394ff815ed657b299bc007dbbea3b7b3c07
Antoine <dumont.antoine27@gmail.com>
2018-04-08 22:27:26 +0200

    modification model Home

--- models/Home.class.php
+++ models/Home.class.php
@@ -10,10 +10,45 @@ class Home extends BaseSql {
 
     private $is_connected = false;
 
+    private $number_of_articles;
+
+
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * @return bool
+     */
+    public function isConnected(): bool
+    {
+        return $this->is_connected;
+    }
 
+    /**
+     * @param bool $is_connected
+     */
+    public function setIsConnected(bool $is_connected)
+    {
+        $this->is_connected = $is_connected;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumberOfArticles()
+    {
+        return $this->number_of_articles;
+    }
+
+    /**
+     * @param mixed $number_of_articles
+     */
+    public function setNumberOfArticles($number_of_articles)
+    {
+        $this->number_of_articles = $number_of_articles;
+    }
+
+    
 }
