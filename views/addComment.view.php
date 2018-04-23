a3866041ca9104e68e923a500670c769e5d61fcf
SylvainC <sylvain.coutrot@hotmail.fr>
2018-04-23 20:34:14 +0200

    Modif des views (description)
    Ajout de :
    article.view.php
    addComment.view.php
    Modification des liens de index.view.php
    Suppression de l'include de userAdmin

---
+++ views/addComment.view.php
@@ -0,0 +1,15 @@
+
+<main role="main">
+      <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Ajouter un commentaire</h2>
+            <?php $this->addModal("form", $config, $errors); ?>
+
+          </div>
+        <hr>
+
+      </div> <!-- /container -->
+
+    </main>
