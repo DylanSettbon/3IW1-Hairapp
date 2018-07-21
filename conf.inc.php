09446d072d857df7c253c1b1327a3b7db2c2d961
Antoine <dumont.antoine27@gmail.com>
2018-07-21 10:18:37 +0200

    conf.inc vierge

--- conf.inc.php
+++ conf.inc.php
@@ -9,11 +9,11 @@
 
 define("DBUSER","root");
 define("DBHOST","localhost");
-define('DBNAME','La_Bottega_Del_Coiffeur');
+define('DBNAME','');
 define("DBPWD","root");
 define("DBPORT","3306");
 define("DBDRIVER","mysql");
-define('INSTALLED', true );
+define('INSTALLED', false );
 define("DS", "/");
 $scriptName = (dirname($_SERVER["SCRIPT_NAME"]) == "/")?"":dirname($_SERVER["SCRIPT_NAME"]);
 define("DIRNAME", $scriptName.DS);
@@ -21,7 +21,7 @@ define("PICTURES_DIR", DIRNAME."public/img/" );
 
 $list_of_extensions = ['.png', '.gif', '.jpg', '.jpeg'];
 
-define('OPENING_HOUR','09:30');
-define('CLOSING_HOUR','18:30');
-define('DURATION','00:30');
+define('OPENING_HOUR','');
+define('CLOSING_HOUR','');
+define('DURATION','');
 
